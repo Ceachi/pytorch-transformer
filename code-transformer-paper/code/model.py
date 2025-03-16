159e7714e2215a4a64cc2afbbfdaee08adb94e0b
Ceachi Bogdan <ceachi.bogdan@gmail.com>
2025-03-16 13:47:57 +0200

    MHA and MQA

--- code-transformer-paper/code/model.py
+++ code-transformer-paper/code/model.py
@@ -160,7 +160,12 @@ class Encoder(nn.Module):
 
 class DecoderBlock(nn.Module):
 
-    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+    def __init__(self, features: int, 
+                 self_attention_block: MultiHeadAttentionBlock, 
+                 cross_attention_block: MultiHeadAttentionBlock, 
+                 feed_forward_block: FeedForwardBlock, 
+                 dropout: float) -> None:
+        
         super().__init__()
         self.self_attention_block = self_attention_block
         self.cross_attention_block = cross_attention_block
@@ -168,8 +173,8 @@ class DecoderBlock(nn.Module):
         self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
 
     def forward(self, x, encoder_output, src_mask, tgt_mask):
-        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask))
-        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tgt_mask)) #q,k,v,mask
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask)) #k,v (from encoder), q from input decoer
         x = self.residual_connections[2](x, self.feed_forward_block)
         return x
     
